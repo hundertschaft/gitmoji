fb4f38d653dabd319571df1f7261dc6d29e8174f
Dino Scheidt <github@dino-scheidt.de>
2017-08-06 23:06:49 +0200

    🚀 gitmoji.com

--- docs/_next/7a23fb1e-0bbc-46e5-a3c3-cc00207b2713/page/components/gitmojiTheme/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/components/gitmojiTheme', function() {
-            var comp = module.exports=webpackJsonp([5],{366:function(e,o,s){e.exports=s(367)},367:function(e,o,s){"use strict";Object.defineProperty(o,"__esModule",{value:!0});var t={breakpoints:[32,48,64,80],space:[0,4,8,16,32,64,128],fontSizes:[12,14,16,20,24,32,48,64,72,96],weights:[200,400],colors:{black:"#000",white:"#fff"},radius:4,font:'"Avenir Next", Helvetica, sans-serif',monospace:'"SF Mono", "Roboto Mono", Menlo, monospace'};o.default=t}},[366]);
-            return { page: comp.default }
-          })
-        
