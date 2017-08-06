738e4b6274d5c66deaa32acb5978e1465c6755fb
Dino Scheidt <github@dino-scheidt.de>
2017-08-06 22:19:27 +0200

    💄 Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,7 +23,16 @@ export default class LandingPage extends Document {
         return (
             <html>
                 <Head>
-                    <title>🔺 Gitmoji: Emoji Cheat Sheet for Github</title>
+                    <title>Gitmoji: Emoji Cheat Sheet for Github 🚀</title>
+
+                    <link rel="icon" type="image/png" href="static/icon/favicon-16x16.png" sizes="16x16" />
+                    <link rel="icon" type="image/png" href="static/icon/favicon-32x32.png" sizes="32x32" />
+                    <link rel="icon" type="image/png" href="static/icon/favicon-96x96.png" sizes="96x96" />
+                    <link rel="apple-touch-icon" href="static/icon/favicon-120x120.png" />
+                    <link rel="apple-touch-icon" sizes="180x180" href="static/icon/favicon-180x180.png" />
+                    <link rel="apple-touch-icon" sizes="152x152" href="static/icon/favicon-152x152.png" />
+                    <link rel="apple-touch-icon" sizes="167x167" href="static/icon/favicon-167x167.png" />
+
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                     {styleTags}
                 </Head>
